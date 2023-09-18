5bac30ff0772d9c03392261ddd428f463b4d0049
Anuj Upadhyay <anuj.upadhyay@rsystems.com>
2023-09-18 22:20:36 +0530

    dg-redux-toolkit-01-app-structure-and-data-flow - initial config.

---
+++ dg-redux-toolkit-complete/src/store/index.jsx
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import postsReducer from "../features/posts/postsSlice.js";
+
+export const store = configureStore({
+  reducer: {
+    posts: postsReducer,
+  }
+})
+
